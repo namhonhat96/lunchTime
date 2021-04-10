5c5f33ecc7e887faf3c00dd3384a26bfcc8e9890
nnho <nnho@ebay.com>
2021-04-10 16:23:29 -0400

    init app

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import SignUpForm from './components/SignUpForm';
+import './App.css';
+
+const projectID = '3256361b-5f0a-417d-a70f-8ff3bf93c4d0';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+  if( localStorage.getItem('signUp')) return <SignUpForm />;
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
